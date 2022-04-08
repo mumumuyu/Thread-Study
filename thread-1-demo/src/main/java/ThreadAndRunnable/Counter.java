8ec5035ab3a66e4d9a504d6cbc27ceb6385884d6
lgd <1017276522@qq.com>
2022-04-08 22:07:53 +0800

    JMM volatile Study

---
+++ thread-1-demo/src/main/java/ThreadAndRunnable/Counter.java
@@ -0,0 +1,51 @@
+package ThreadAndRunnable;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Description:
+ * @author: LGD
+ * @date:2022/4/8 20:55
+ */
+public class Counter {
+//    public static volatile int num = 0;
+//    //使用CountDownLatch来等待计算线程执行完
+//    static CountDownLatch countDownLatch = new CountDownLatch(30);
+//    public static void main(String []args) throws InterruptedException {
+//        //开启30个线程进行累加操作
+//        for(int i=0;i<30;i++){
+//            new Thread(){
+//                public void run(){
+//                    for(int j=0;j<10000;j++){
+//                        num++;//自加操作
+//                    }
+//                    countDownLatch.countDown();
+//                }
+//            }.start();
+//        }
+//        //等待计算线程执行完
+//        countDownLatch.await();
+//        System.out.println(num);
+//    }
+    //使用原子操作类
+    public static AtomicInteger num = new AtomicInteger(0);
+    //使用CountDownLatch来等待计算线程执行完
+    static CountDownLatch countDownLatch = new CountDownLatch(30);
+    public static void main(String []args) throws InterruptedException {
+        //开启30个线程进行累加操作
+        for(int i=0;i<30;i++){
+            new Thread(){
+                public void run(){
+                    for(int j=0;j<10000;j++){
+                        num.incrementAndGet();//原子性的num++,通过循环CAS方式
+                    }
+                    countDownLatch.countDown();
+                }
+            }.start();
+        }
+        //等待计算线程执行完
+        countDownLatch.await();
+        System.out.println(num);
+    }
+}
--- thread-1-demo/src/main/java/ThreadAndRunnable/RunnableTest2.java
+++ thread-1-demo/src/main/java/ThreadAndRunnable/RunnableTest2.java
@@ -10,13 +10,13 @@ public class RunnableTest2 implements Runnable{
                 System.out.println("手慢了，票子拿完咯");
                 break;
             }else{
-                //延迟0.2秒再让你抢
-                try {
-                    Thread.sleep(200);
-                }catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-                System.out.println(Thread.currentThread().getName()+"拿到了"+tickerName--+"张票");
+//                延迟0.2秒再让你抢
+//                try {
+//                    Thread.sleep(200);
+//                }catch (InterruptedException e){
+//                    e.printStackTrace();
+//                }
+                System.out.println(Thread.currentThread().getName()+"拿到了"+tickerName-- +"张票");
             }
         }
     }
@@ -26,5 +26,6 @@ public class RunnableTest2 implements Runnable{
         new Thread(runnableTest2,"小明").start();
         new Thread(runnableTest2,"小红").start();
         new Thread(runnableTest2,"黄牛").start();
+        new Thread(runnableTest2,"红牛").start();
     }
 }
